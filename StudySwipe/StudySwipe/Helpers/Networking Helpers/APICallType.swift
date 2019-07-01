6a51a06e764185914432f5853a737db501868884
Ben Hakes <bhhakes@gmail.com>
2019-07-01 16:40:52 -0500

    add networking controller and add. model structs

---
+++ StudySwipe/StudySwipe/Helpers/Networking Helpers/APICallType.swift
@@ -0,0 +1,17 @@
+//
+//  APICallType.swift
+//  StudySwipe
+//
+//  Created by Benjamin Hakes on 7/1/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+import Foundation
+
+enum APICallType: String, Codable {
+    
+    // questions
+    case GETQuestions
+
+}
+
--- StudySwipe/StudySwipe/Model/Question/QuestionRepresentation.swift
+++ StudySwipe/StudySwipe/Model/Question/QuestionRepresentation.swift
@@ -8,20 +8,70 @@
 
 import Foundation
 
-struct QuestionRepresentation {
-    var answer: String
-    var category: Category
-    var difficulty: Difficulty
-    var question: String
-    var questionID: UUID
-    var track: Track
+struct QuestionRepresentation: Codable {
+    var answer: GSXAnswer
+    var category: GSXCategory
+    var difficulty: GSXDifficulty
+    var question: GSXQuestion
+    var questionID: GSXQuestionID
+    var track: GSXTrack
     
     enum CodingKeys: String, CodingKey {
-        case answer
-        case category
-        case difficulty
-        case question
-        case questionID
-        case track
+    
+        case answer = "gsx$answer"
+        case category = "gsx$category"
+        case difficulty = "gsx$difficulty"
+        case question = "gsx$question"
+        case questionID = "gsx$questionid"
+        case track = "gsx$track"
+    }
+    
+}
+
+struct GSXAnswer: Codable {
+    let answer: String
+    
+    enum CodingKeys: String, CodingKey {
+        case answer = "$t"
+    }
+}
+
+struct GSXCategory: Codable {
+    let category: Category
+    
+    enum CodingKeys: String, CodingKey {
+        case category = "$t"
+    }
+}
+
+struct GSXDifficulty: Codable {
+    let difficulty: Difficulty
+    
+    enum CodingKeys: String, CodingKey {
+        case difficulty = "$t"
+    }
+}
+
+struct GSXQuestion: Codable {
+    let question: String
+    
+    enum CodingKeys: String, CodingKey {
+        case question = "$t"
+    }
+}
+
+struct GSXQuestionID: Codable {
+    let questionID: UUID
+    
+    enum CodingKeys: String, CodingKey {
+        case questionID = "$t"
+    }
+}
+
+struct GSXTrack: Codable {
+    let track: Track
+    
+    enum CodingKeys: String, CodingKey {
+        case track = "$t"
     }
 }
