2fddaa0db38bc4c94e5b8e7f9398af009fc6eb2d
Ben Hakes <bhhakes@gmail.com>
2019-08-17 18:10:03 -0400

    remove DMCCollectionView

--- StudySwipe/StudySwipe/Views/Test/TestSetupViewController.swift
+++ StudySwipe/StudySwipe/Views/Test/TestSetupViewController.swift
@@ -1,209 +1,209 @@
+////
+////  TestSetupViewController.swift
+////  StudySwipe
+////
+////  Created by Dillon McElhinney on 7/2/19.
+////  Copyright © 2019 Dillon McElhinney. All rights reserved.
+////
 //
-//  TestSetupViewController.swift
-//  StudySwipe
+//import UIKit
 //
-//  Created by Dillon McElhinney on 7/2/19.
-//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//class TestSetupViewController: UIViewController {
 //
-
-import UIKit
-
-class TestSetupViewController: UIViewController {
-    
-    let coreDataFetchController = CoreDataFetchController()
-    
-    var quoteLabel: UILabel!
-    var authorLabel: UILabel!
-    var masteredLabel: UILabel!
-    var masteredSwitch: UISwitch!
-    var segmentedControl: UISegmentedControl!
-    var questionNumberLabel: UILabel!
-    var questionSlider: UISlider!
-    
-    private var themedStatusBarStyle: UIStatusBarStyle?
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return themedStatusBarStyle ?? super.preferredStatusBarStyle
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setupViews()
-        setUpTheming()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        updateQuote()
-    }
-    
-    // MARK: - UI Actions
-    @objc func startCustomTest() {
-        let number = Int(questionSlider.value)
-        let difficulty = Difficulty.allCases[segmentedControl.selectedSegmentIndex]
-        startTest(difficulty: difficulty, numberOfQuestions: number)
-    }
-    
-    @objc func updateQuestionNumberLabel() {
-        let number = Int(questionSlider.value)
-        questionNumberLabel.text = "\(number) Questions"
-        
-    }
-    
-    // MARK: - Private Methods
-    private func setupViews() {
-        
-        // Set up Title Label
-        let titleLabel = DMCLabel()
-        titleLabel.text = "Take a Test"
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 48)
-        
-        titleLabel.constrainToSuperView(view, top: 20, leading: 20, trailing: 20)
-        
-        // Set up Main Stack
-        let mainStack = UIStackView()
-        mainStack.axis = .vertical
-        mainStack.spacing = 8
-        mainStack.alignment = .center
-        
-        mainStack.constrainToSuperView(view, bottom: 20, leading: 20, trailing: 20)
-        mainStack.constrainToSiblingView(titleLabel, below: 20)
-        
-        let spacer1 = DMCView()
-        mainStack.addArrangedSubview(spacer1)
-        
-        // Set up Quote Stack
-        let quoteStack = UIStackView()
-        quoteStack.axis = .vertical
-        quoteStack.spacing = 12
-        
-        mainStack.addArrangedSubview(quoteStack)
-        quoteStack.constrain(width: 300)
-        
-        quoteLabel = UILabel()
-        
-        quoteLabel.textColor = .fadedTextColor
-        quoteLabel.textAlignment = .center
-        quoteLabel.numberOfLines = 0
-        quoteLabel.font = UIFont.italicSystemFont(ofSize: 18)
-        
-        quoteStack.addArrangedSubview(quoteLabel)
-        
-        authorLabel = UILabel()
-        
-        authorLabel.textColor = .fadedTextColor
-        authorLabel.textAlignment = .right
-        authorLabel.font = UIFont.systemFont(ofSize: 18, weight: .regular)
-        
-        quoteStack.addArrangedSubview(authorLabel)
-        
-        let spacer2 = DMCView()
-        mainStack.addArrangedSubview(spacer2)
-        
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.spacing = 12
-        
-        stackView.constrain(width: 240)
-        mainStack.addArrangedSubview(stackView)
-        
-        let spacer3 = DMCView()
-        mainStack.addArrangedSubview(spacer3)
-        
-        // Set up Difficulty Segmented Control
-        let difficulties = Difficulty.allCases.map { $0.title() }
-        segmentedControl = UISegmentedControl(items: difficulties)
-        segmentedControl.selectedSegmentIndex = 0
-        segmentedControl.tintColor = .accentColor
-        
-        stackView.addArrangedSubview(segmentedControl)
-        
-        // Set up Question Count Slider
-        questionNumberLabel = UILabel()
-        questionNumberLabel.font = UIFont.preferredFont(forTextStyle: .title2)
-        questionNumberLabel.textAlignment = .center
-        questionNumberLabel.textColor = .fadedTextColor
-        stackView.addArrangedSubview(questionNumberLabel)
-        
-        let sliderContainer = UIView()
-        sliderContainer.backgroundColor = .accentColor
-        sliderContainer.layer.cornerRadius = 8
-        stackView.addArrangedSubview(sliderContainer)
-        
-        questionSlider = UISlider()
-        questionSlider.minimumValue = 5
-        questionSlider.maximumValue = 30
-        questionSlider.setValue(10, animated: false)
-        questionSlider.tintColor = .white
-        questionSlider.maximumTrackTintColor = .white
-        
-        questionSlider.addTarget(self, action: #selector(updateQuestionNumberLabel), for: .primaryActionTriggered)
-        
-        updateQuestionNumberLabel()
-        
-        questionSlider.constrainToSuperView(sliderContainer, top: 8, bottom: 8, leading: 20, trailing: 20)
-        
-        // add masterLabel & switch
-        masteredLabel = UILabel()
-        masteredLabel.numberOfLines = 1
-        masteredLabel.textColor = .fadedTextColor
-        masteredLabel.textAlignment = .left
-        masteredLabel.text = "Include mastered cards: "
-        masteredLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
-        
-        masteredSwitch = UISwitch()
-        masteredSwitch.onTintColor = .accentColor
-        masteredSwitch.isOn = true
-        
-        let isMasteredStackView = UIStackView(arrangedSubviews: [masteredLabel, masteredSwitch])
-        isMasteredStackView.axis = .horizontal
-        isMasteredStackView.spacing = 12
-        
-        isMasteredStackView.constrain(width: 240)
-        stackView.addArrangedSubview(isMasteredStackView)
-        
-        // Set up Start Test Button
-        let startTestButton = UIButton(type: .system)
-        startTestButton.setTitle("Start Test", for: .normal)
-        startTestButton.setTitleColor(.white, for: .normal)
-        startTestButton.backgroundColor = .accentColor
-        startTestButton.addTarget(self, action: #selector(startCustomTest), for: .touchUpInside)
-        startTestButton.layer.cornerRadius = 8
-        
-        startTestButton.constrain(width: 120)
-        mainStack.addArrangedSubview(startTestButton)
-        
-        let spacer4 = DMCView()
-        mainStack.addArrangedSubview(spacer4)
-        
-        spacer1.constrainToSiblingView(spacer2, equalHeight: 0)
-        spacer2.constrainToSiblingView(spacer3, equalHeight: 0)
-        spacer3.constrainToSiblingView(spacer4, equalHeight: 0)
-    }
-    
-    private func updateQuote() {
-        let (author, quote) = Quotes.getNewQuote()
-        quoteLabel.text = "\"\(quote)\""
-        authorLabel.text = "– \(author)"
-    }
-    
-    private func startTest(difficulty: Difficulty = .All, numberOfQuestions number: Int) {
-        let (test, observation) = coreDataFetchController.makeTestAndObservation(with: "New \(number) Question Test", difficulties: [difficulty], count: number, random: true, notMasteredOnly: !masteredSwitch.isOn)
-        let testViewController = TestViewController()
-        testViewController.coreDataFetchController = coreDataFetchController
-        testViewController.testObservation = observation
-        testViewController.test = test
-        
-        present(testViewController, animated: true)
-    }
-}
-
-extension TestSetupViewController: Themed {
-    func applyTheme(_ theme: AppTheme) {
-        themedStatusBarStyle = theme.statusBarStyle
-        setNeedsStatusBarAppearanceUpdate()
-        
-        view.backgroundColor = theme.backgroundColor
-    }
-}
+//    let coreDataFetchController = CoreDataFetchController()
+//
+//    var quoteLabel: UILabel!
+//    var authorLabel: UILabel!
+//    var masteredLabel: UILabel!
+//    var masteredSwitch: UISwitch!
+//    var segmentedControl: UISegmentedControl!
+//    var questionNumberLabel: UILabel!
+//    var questionSlider: UISlider!
+//
+//    private var themedStatusBarStyle: UIStatusBarStyle?
+//    override var preferredStatusBarStyle: UIStatusBarStyle {
+//        return themedStatusBarStyle ?? super.preferredStatusBarStyle
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        setupViews()
+//        setUpTheming()
+//    }
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//
+//        updateQuote()
+//    }
+//
+//    // MARK: - UI Actions
+//    @objc func startCustomTest() {
+//        let number = Int(questionSlider.value)
+//        let difficulty = Difficulty.allCases[segmentedControl.selectedSegmentIndex]
+//        startTest(difficulty: difficulty, numberOfQuestions: number)
+//    }
+//
+//    @objc func updateQuestionNumberLabel() {
+//        let number = Int(questionSlider.value)
+//        questionNumberLabel.text = "\(number) Questions"
+//
+//    }
+//
+//    // MARK: - Private Methods
+//    private func setupViews() {
+//
+//        // Set up Title Label
+//        let titleLabel = DMCLabel()
+//        titleLabel.text = "Take a Test"
+//        titleLabel.font = UIFont.boldSystemFont(ofSize: 48)
+//
+//        titleLabel.constrainToSuperView(view, top: 20, leading: 20, trailing: 20)
+//
+//        // Set up Main Stack
+//        let mainStack = UIStackView()
+//        mainStack.axis = .vertical
+//        mainStack.spacing = 8
+//        mainStack.alignment = .center
+//
+//        mainStack.constrainToSuperView(view, bottom: 20, leading: 20, trailing: 20)
+//        mainStack.constrainToSiblingView(titleLabel, below: 20)
+//
+//        let spacer1 = DMCView()
+//        mainStack.addArrangedSubview(spacer1)
+//
+//        // Set up Quote Stack
+//        let quoteStack = UIStackView()
+//        quoteStack.axis = .vertical
+//        quoteStack.spacing = 12
+//
+//        mainStack.addArrangedSubview(quoteStack)
+//        quoteStack.constrain(width: 300)
+//
+//        quoteLabel = UILabel()
+//
+//        quoteLabel.textColor = .fadedTextColor
+//        quoteLabel.textAlignment = .center
+//        quoteLabel.numberOfLines = 0
+//        quoteLabel.font = UIFont.italicSystemFont(ofSize: 18)
+//
+//        quoteStack.addArrangedSubview(quoteLabel)
+//
+//        authorLabel = UILabel()
+//
+//        authorLabel.textColor = .fadedTextColor
+//        authorLabel.textAlignment = .right
+//        authorLabel.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+//
+//        quoteStack.addArrangedSubview(authorLabel)
+//
+//        let spacer2 = DMCView()
+//        mainStack.addArrangedSubview(spacer2)
+//
+//        let stackView = UIStackView()
+//        stackView.axis = .vertical
+//        stackView.spacing = 12
+//
+//        stackView.constrain(width: 240)
+//        mainStack.addArrangedSubview(stackView)
+//
+//        let spacer3 = DMCView()
+//        mainStack.addArrangedSubview(spacer3)
+//
+//        // Set up Difficulty Segmented Control
+//        let difficulties = Difficulty.allCases.map { $0.title() }
+//        segmentedControl = UISegmentedControl(items: difficulties)
+//        segmentedControl.selectedSegmentIndex = 0
+//        segmentedControl.tintColor = .accentColor
+//
+//        stackView.addArrangedSubview(segmentedControl)
+//
+//        // Set up Question Count Slider
+//        questionNumberLabel = UILabel()
+//        questionNumberLabel.font = UIFont.preferredFont(forTextStyle: .title2)
+//        questionNumberLabel.textAlignment = .center
+//        questionNumberLabel.textColor = .fadedTextColor
+//        stackView.addArrangedSubview(questionNumberLabel)
+//
+//        let sliderContainer = UIView()
+//        sliderContainer.backgroundColor = .accentColor
+//        sliderContainer.layer.cornerRadius = 8
+//        stackView.addArrangedSubview(sliderContainer)
+//
+//        questionSlider = UISlider()
+//        questionSlider.minimumValue = 5
+//        questionSlider.maximumValue = 30
+//        questionSlider.setValue(10, animated: false)
+//        questionSlider.tintColor = .white
+//        questionSlider.maximumTrackTintColor = .white
+//
+//        questionSlider.addTarget(self, action: #selector(updateQuestionNumberLabel), for: .primaryActionTriggered)
+//
+//        updateQuestionNumberLabel()
+//
+//        questionSlider.constrainToSuperView(sliderContainer, top: 8, bottom: 8, leading: 20, trailing: 20)
+//
+//        // add masterLabel & switch
+//        masteredLabel = UILabel()
+//        masteredLabel.numberOfLines = 1
+//        masteredLabel.textColor = .fadedTextColor
+//        masteredLabel.textAlignment = .left
+//        masteredLabel.text = "Include mastered cards: "
+//        masteredLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+//
+//        masteredSwitch = UISwitch()
+//        masteredSwitch.onTintColor = .accentColor
+//        masteredSwitch.isOn = true
+//
+//        let isMasteredStackView = UIStackView(arrangedSubviews: [masteredLabel, masteredSwitch])
+//        isMasteredStackView.axis = .horizontal
+//        isMasteredStackView.spacing = 12
+//
+//        isMasteredStackView.constrain(width: 240)
+//        stackView.addArrangedSubview(isMasteredStackView)
+//
+//        // Set up Start Test Button
+//        let startTestButton = UIButton(type: .system)
+//        startTestButton.setTitle("Start Test", for: .normal)
+//        startTestButton.setTitleColor(.white, for: .normal)
+//        startTestButton.backgroundColor = .accentColor
+//        startTestButton.addTarget(self, action: #selector(startCustomTest), for: .touchUpInside)
+//        startTestButton.layer.cornerRadius = 8
+//
+//        startTestButton.constrain(width: 120)
+//        mainStack.addArrangedSubview(startTestButton)
+//
+//        let spacer4 = DMCView()
+//        mainStack.addArrangedSubview(spacer4)
+//
+//        spacer1.constrainToSiblingView(spacer2, equalHeight: 0)
+//        spacer2.constrainToSiblingView(spacer3, equalHeight: 0)
+//        spacer3.constrainToSiblingView(spacer4, equalHeight: 0)
+//    }
+//
+//    private func updateQuote() {
+//        let (author, quote) = Quotes.getNewQuote()
+//        quoteLabel.text = "\"\(quote)\""
+//        authorLabel.text = "– \(author)"
+//    }
+//
+//    private func startTest(difficulty: Difficulty = .All, numberOfQuestions number: Int) {
+//        let (test, observation) = coreDataFetchController.makeTestAndObservation(with: "New \(number) Question Test", difficulties: [difficulty], count: number, random: true, notMasteredOnly: !masteredSwitch.isOn)
+//        let testViewController = TestViewController()
+//        testViewController.coreDataFetchController = coreDataFetchController
+//        testViewController.testObservation = observation
+//        testViewController.test = test
+//
+//        present(testViewController, animated: true)
+//    }
+//}
+//
+//extension TestSetupViewController: Themed {
+//    func applyTheme(_ theme: AppTheme) {
+//        themedStatusBarStyle = theme.statusBarStyle
+//        setNeedsStatusBarAppearanceUpdate()
+//
+//        view.backgroundColor = theme.backgroundColor
+//    }
+//}
